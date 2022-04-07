e843beadb56f422e66a96873852a0dea836740ad
BeetrootWang <wzt12345678@gmail.com>
2022-04-06 21:50:37 -0400

    debug

--- main.py
+++ main.py
@@ -12,12 +12,14 @@ def F_LR(x, cov_a, x_star, var_epsilon):
 # rng: random generator
 # x_prev: initial guess
 # n: number of iterations
-def run_SGD_LR_O(rng, x_star, x_prev, n):
+def run_SGD_LR_O(seed, x_star, x_prev, n):
+    rng = np.random.default_rng(seed)
     d = len(x_prev)
     x_history = []
     a_n_history = rng.normal(0, 1, (n,d))
     epsilon_n_history = rng.normal(0,var_epsilon,n)
     b_n_history = []
+    print(f'Seed: [{seed}/100] ...')
     for iter_num in range(n):
         # sample data
         a_n = a_n_history[iter_num,:]
@@ -32,8 +34,8 @@ def run_SGD_LR_O(rng, x_star, x_prev, n):
         x_history.append(x_n)
         b_n_history.append(b_n)
         # output every 1000 iter
-        if iter_num % int(n/10) == int(n/10-1):
-            print(f'Iter \t[{iter_num + 1}/{n}]\t\t finished')
+        # if iter_num % int(n/10) == int(n/10-1):
+        #     print(f'Seed: \t [{seed}/100]\t Iter \t[{iter_num + 1}/{n}]\t\t finished')
     x_out = np.mean(x_history, axis=0)
     return x_out, a_n_history, b_n_history
 
@@ -44,6 +46,8 @@ def bootstrap_CI(x_0, n, R, a_n_history, b_n_history):
     rng_b = np.random.default_rng(1)  # random generator for bootstrap experiment
     bootstrap_samples_all = rng_b.integers(0, n, (R,n))  # bootstrap_samples[i] is the index of data for i-th iteration
     for r in range(1, R + 1):
+        if r%5==0:
+            print(f'---> bootstrap [{r}/{R}] Done')
         # which is selected uniformly from given data
         # SGD on bootstrap samples
         x_prev = x_0
@@ -61,8 +65,8 @@ def bootstrap_CI(x_0, n, R, a_n_history, b_n_history):
             # recording
             x_history.append(x_n)
             # output every 1000 iter
-            if iter_num % int(n/10) == int(n/10-1):
-                print(f'R: \t[{r}/{R}]\t Iter \t[{iter_num + 1}/{n}]\t\t finished')
+            # if iter_num % int(n/10) == int(n/10-1):
+            #     print(f'R: \t[{r}/{R}]\t Iter \t[{iter_num + 1}/{n}]\t\t finished')
         bootstrap_output_history.append(np.mean(x_history, axis=0))
 
     # bootstrap true solution
@@ -91,12 +95,12 @@ if __name__ == '__main__':
     # basic setting
     var_epsilon = 1  # variance for noise in linear regression
     d = 5  # d = 5,20,100,200
-    n = int(1e5)  # sample size
-    eta = 1e0
+    n = int(1e3)  # sample size
+    eta = 5e-1
     alpha = 0.501  # step size eta_i = eta * i^{-alpha}
     x_star = np.linspace(0, 1, d)  # optimal solution
     x_0 = np.zeros(d)  # initial guess
-    R = 3 # number of bootstrap
+    R = 10 # number of bootstrap
     num_trials = 100
 
     # mean and variance for generating a_i
@@ -112,22 +116,29 @@ if __name__ == '__main__':
     # set random seed for original samples
     mean_len_history = []
     std_len_history = []
+    len_history = []
     cov_history = []
-    for seed in range(num_trials):
-        rng = np.random.default_rng(seed)
-        x_out, a_n_history, b_n_history = run_SGD_LR_O(rng, x_star, x_0, n)
+    for seed in range(1,1+num_trials):
+        x_out, a_n_history, b_n_history = run_SGD_LR_O(seed, x_star, x_0, n)
         x_r, CI_radius = bootstrap_CI(x_out, n, R, a_n_history, b_n_history)
 
         mean_Len = np.mean(CI_radius*2)
         std_Len = np.std(CI_radius*2)
+        len_history.append(CI_radius*2)
         mean_len_history.append(mean_Len)
         std_len_history.append(std_Len)
         cover = [1 if abs(x_out[ii]-x_r[ii])<= CI_radius[ii] else 0 for ii in range(len(x_out))]
         cov_history.append(cover)
 
-    for seed in range(num_trials):
+    for seed in range(1,1+num_trials):
         # debug code
         print('*'*20)
-        print(f'Len: {mean_len_history[seed]} ({std_len_history[seed]})')
+        print(f'Len: {mean_len_history[seed-1]} ({std_len_history[seed-1]})')
     print(np.mean(cov_history))
     # import pdb; pdb.set_trace()
+
+    f = open(f'Result.txt','a')
+    f.write('----->\n')
+    f.write(f'\tCov Rate (%) \t d: {d} \t n: {n} \t R: {R} \t eta_0: {eta} \t alpha: {alpha} \t # Trials: {num_trials}\n')
+    f.write(f'\tAvg Len: {np.mean(len_history)} \t ({np.std(len_history)}) \t Cov Rate: {np.mean(cov_history)} \t ({np.std(cov_history)})\n')
+    f.close()
