3f4aa00394d4b1cf708e8e30f9e54bf78e6b98d0
wzt <wzt12345678@gmail.com>
2022-04-12 15:02:56 -0400

    experiments

--- test_d1.py
+++ test_d1.py
@@ -9,10 +9,10 @@ if __name__ == '__main__':
     # eta = 1e-3
     alpha = 0.501  # step size eta_i = eta * i^{-alpha}
     # R = 2  # number of bootstrap
-    num_trials = 100
+    num_trials = 500
 
-    for R in [50]:
-        for d in [1,5,20,200]:
+    for R in [2]:
+        for d in [11]:
             x_star = np.linspace(0, 1, d)  # optimal solution
             x_0 = np.zeros(d)  # initial guess
             for eta in [5e-1,5e-2,5e-3]:
