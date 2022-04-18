823805dd7c8b34ec01310748daa819b4da586c93
wzt <wzt12345678@gmail.com>
2022-04-18 14:59:44 -0400

    experiments

--- test_d1.py
+++ test_d1.py
@@ -1,4 +1,4 @@
-from main import main_experiments, main_experiments_parallel
+from main import main_experiments, main_experiments_parallel, main_experiments_parallel_std
 import numpy as np
 
 if __name__ == '__main__':
@@ -11,9 +11,9 @@ if __name__ == '__main__':
     # R = 2  # number of bootstrap
     num_trials = 500
 
-    for R in [2]:
-        for d in [11]:
+    for R in [2, 10, 50]:
+        for d in [1, 11, 200]:
             x_star = np.linspace(0, 1, d)  # optimal solution
             x_0 = np.zeros(d)  # initial guess
-            for eta in [5e-1,5e-2,5e-3]:
-                main_experiments_parallel(d, n, eta, alpha, x_star, x_0, R, var_epsilon, num_trials)
+            for eta in [1e-1,1e-2]:
+                main_experiments_parallel_std(d, n, eta, alpha, x_star, x_0, R, var_epsilon, num_trials)
