fix bug. Delete wrong results.

main.py
@@ -1,6 +1,8 @@
 # import packages
+import pdb
+
 import numpy as np
-from scipy.stats import t
+from scipy.stats import t, norm
 from joblib import Parallel, delayed
 
 # F for linear regression
@@ -8,7 +10,6 @@ from joblib import Parallel, delayed
 #        = 1/2 (x-x_star)@cov_a@(x-x_star) + var_epsilon
 def F_LR(x, cov_a, x_star, var_epsilon):
     return .5 * (x - x_star) @ cov_a @ (x - x_star) + var_epsilon
-
 
 # SGD original loop (a_n, b_n) iid sample from normal
 # rng: random generator
@@ -40,7 +41,6 @@ def run_SGD_LR_O(seed, x_star, x_prev, n, eta, var_epsilon, alpha):
     x_out = np.mean(x_history, axis=0)
     # print(x_out)
     return x_out, a_n_history, b_n_history
-
 
 # SGD bootstrap loop
 # compute bootstrap confidence interval
@@ -95,7 +95,6 @@ def bootstrap_CI(x_0, n, R, a_n_history, b_n_history, eta, alpha):
     CI_radius = np.array(CI_radius)
     return CI_radius
 
-
 def run_SGD_LR_std_O(seed, x_star, x_prev, n, eta, var_epsilon, alpha):
     rng = np.random.default_rng(seed)
     d = len(x_prev)
@@ -123,6 +122,38 @@ def run_SGD_LR_std_O(seed, x_star, x_prev, n, eta, var_epsilon, alpha):
     # print(x_out)
     return x_out, a_n_history, b_n_history
 
+def run_SGD_LR_plug_in(seed, x_star, x_prev, n, eta, var_epsilon, alpha, delta=1e-6):
+    rng = np.random.default_rng(seed)
+    d = len(x_prev)
+    x_history = []
+    a_n_history = rng.normal(0, 1, (n, d))
+    epsilon_n_history = rng.normal(0, var_epsilon, n)
+    for iter_num in range(n):
+        # sample data
+        a_n = a_n_history[iter_num, :]
+        epsilon_n = epsilon_n_history[iter_num]
+        b_n = a_n @ x_star + epsilon_n
+        # update learning rate
+        eta_n = eta * (1 + iter_num) ** (-alpha)
+        # update rule
+        x_n = x_prev - eta_n * (a_n @ x_prev - b_n) * a_n
+        x_prev = x_n
+        # recording
+        x_history.append(x_n)
+    x_out = np.mean(x_history, axis=0)
+
+    # Compute \tilde A_n and S_n to get sigma hat
+    # Use sigma hat to get CI_radius
+    epsilon_n_history = np.reshape(epsilon_n_history, (n,1))
+    hat_S = (epsilon_n_history * a_n_history).T @ (epsilon_n_history * a_n_history) / n
+    hat_A = a_n_history.T @ a_n_history / n
+    # import pdb; pdb.set_trace()
+    w,V = np.linalg.eig(hat_A)
+    W = np.diag(w * (w>delta) + delta * (w<=delta))
+    tilde_A_inv = np.linalg.inv(V @ W @ V.T)
+    z = norm.ppf(0.975)
+    CI_radius = z * np.sqrt(np.diag(tilde_A_inv @ hat_S @ tilde_A_inv)) / np.sqrt(n)
+    return x_out, CI_radius
 
 # SGD bootstrap loop
 # compute bootstrap confidence interval
@@ -177,7 +208,7 @@ def bootstrap_CI_std(x_0, n, R, a_n_history, b_n_history, eta, alpha):
     CI_radius = np.array(CI_radius)
     return CI_radius
 
-
+# no longer useful
 def main_experiments(d, n, eta, alpha, x_star, x_0, R, var_epsilon, num_trials):
     # mean and variance for generating a_i
     # identity covariance matrix case
@@ -246,6 +277,16 @@ def main_loop_std(seed, x_star, x_0, n, R, eta, var_epsilon, alpha, num_trials):
 
     return mean_Len, std_Len, cover, CI_radius*2, x_out
 
+def main_loop_plug_in(seed, x_star, x_0, n, eta, var_epsilon, alpha, num_trials):
+    print(f'Seed: [{seed}/{num_trials}] ...')
+    x_out, CI_radius = run_SGD_LR_plug_in(seed, x_star, x_0, n, eta, var_epsilon, alpha)
+
+    mean_Len = np.mean(CI_radius * 2)
+    std_Len = np.std(CI_radius * 2)
+    cover = [1 if abs(x_out[ii] - x_star[ii]) <= CI_radius[ii] else 0 for ii in range(len(x_out))]
+
+    return mean_Len, std_Len, cover, CI_radius*2, x_out
+
 def main_experiments_parallel(d, n, eta, alpha, x_star, x_0, R, var_epsilon, num_trials):
     # mean and variance for generating a_i
     # identity covariance matrix case
@@ -282,7 +323,7 @@ def main_experiments_parallel(d, n, eta, alpha, x_star, x_0, R, var_epsilon, num
     f = open(f'Result_{d}.txt', 'a')
     f.write('----->\n')
     f.write(
-        f'\t Cov Rate: {np.mean(cov_history)} \t ({np.std(cov_history)}) \tAvg Len: {np.mean(len_history)} \t ({np.std(len_history)}) \n')
+        f'\t Cov Rate: {np.mean(cov_history)} \t ({np.std(cov_history)}) \tAvg Len: {np.mean(len_history)} \t ({np.std(len_history)/num_trials}) \n')
     f.write(f'\t d: {d} \t n: {n} \t R: {R} \t eta_0: {eta} \t alpha: {alpha} \t # Trials: {num_trials}\n')
     f.write(f'\t True solution:           [')
     for ii in range(d):
@@ -308,7 +349,6 @@ def main_experiments_parallel(d, n, eta, alpha, x_star, x_0, R, var_epsilon, num
     f.close()
 
     return
-
 
 def main_experiments_parallel_std(d, n, eta, alpha, x_star, x_0, R, var_epsilon, num_trials):
     # mean and variance for generating a_i
@@ -346,7 +386,7 @@ def main_experiments_parallel_std(d, n, eta, alpha, x_star, x_0, R, var_epsilon,
     f = open(f'Result_std_{d}.txt', 'a')
     f.write('----->\n')
     f.write(
-        f'\t Cov Rate: {np.mean(cov_history)} \t ({np.std(cov_history)}) \tAvg Len: {np.mean(len_history)} \t ({np.std(len_history)}) \n')
+        f'\t Cov Rate: {np.mean(cov_history)} \t ({np.std(cov_history)}) \tAvg Len: {np.mean(len_history)} \t ({np.std(len_history)/num_trials}) \n')
     f.write(f'\t d: {d} \t n: {n} \t R: {R} \t eta_0: {eta} \t alpha: {alpha} \t # Trials: {num_trials}\n')
     f.write(f'\t True solution:           [')
     for ii in range(d):
@@ -373,6 +413,69 @@ def main_experiments_parallel_std(d, n, eta, alpha, x_star, x_0, R, var_epsilon,
 
     return
 
+def main_experiments_parallel_plug_in(d, n, eta, alpha, x_star, x_0, var_epsilon, num_trials):
+    # mean and variance for generating a_i
+    # identity covariance matrix case
+    #
+    # linear regression model:
+    # b_i = x_star^\top a_i + \epsilon_i
+    mean_a = np.zeros(d)
+    cov_a = np.eye(d)
+    Asy_cov = np.eye(d)  # asymptotic covariance matrix
+
+    # SGD origial loop
+    # set random seed for original samples
+    results = Parallel(n_jobs=32)(delayed(main_loop_plug_in)(seed, x_star, x_0, n, eta, var_epsilon, alpha, num_trials) for seed in range(1, 1+num_trials))
+    # main_loop_plug_in(1, x_star, x_0, n, eta, var_epsilon, alpha, num_trials)
+    mean_len_history = []
+    std_len_history = []
+    len_history = []
+    cov_history = []
+    x_out_history = []
+    for ii in range(num_trials):
+        mean_len_history.append(results[ii][0])
+        std_len_history.append(results[ii][1])
+        cov_history.append(results[ii][2])
+        len_history.append(results[ii][3])
+        x_out_history.append(results[ii][4])
+
+
+    for seed in range(1, 1 + num_trials):
+        # debug code
+        print('*' * 20)
+        print(f'Len: {mean_len_history[seed - 1]:.6f} ({std_len_history[seed - 1]:.10f})')
+    print(np.mean(cov_history))
+    # import pdb; pdb.set_trace()
+
+    f = open(f'Result_PI_{d}.txt', 'a')
+    f.write('----->\n')
+    f.write(
+        f'\t Cov Rate: {np.mean(cov_history)} \t ({np.std(cov_history)}) \tAvg Len: {np.mean(len_history)} \t ({np.std(len_history)/num_trials}) \n')
+    f.write(f'\t d: {d} \t n: {n} \t R: N.A. \t eta_0: {eta} \t alpha: {alpha} \t # Trials: {num_trials}\n')
+    f.write(f'\t True solution:           [')
+    for ii in range(d):
+        f.write(f'{x_star[ii]:.6f}, ')
+    f.write(']\n')
+    f.write(f'\t center in last trial:    [')
+    for ii in range(d):
+        f.write(f'{x_out_history[-1][ii]:.6f}, ')
+    f.write(']\n')
+    f.write(f'\t CI UB in the last trial: [')
+    for ii in range(d):
+        f.write(f'{len_history[-1][ii] + x_out_history[-1][ii]:.6f}, ')
+    f.write(']\n')
+    f.write(f'\t CI LB in the last trial: [')
+    for ii in range(d):
+        f.write(f'{-len_history[-1][ii] + x_out_history[-1][ii]:.6f}, ')
+    f.write(']\n')
+    # f.write(f'\t Cover in the last trial: [')
+    # for ii in range(d):
+    #     f.write(f'{(cov_history)[-1][ii]:.0f}       , ')
+    # f.write(']\n')
+
+    f.close()
+
+    return
 
 if __name__ == '__main__':
     # basic setting
@@ -390,4 +493,4 @@ if __name__ == '__main__':
         # main_experiments(d, n, eta, alpha, x_star, x_0, R, var_epsilon, num_trials)
         main_experiments_parallel(d, n, eta, alpha, x_star, x_0, R, var_epsilon, num_trials)
 
-    # TODO: t-distribution; d=1; sensitivity (eta, X_0, alpha);
+

test_d1_plug_in.py
@@ -1,16 +1,15 @@
-from main import main_experiments
+from main import main_experiments, main_experiments_parallel_plug_in
 import numpy as np
 
 if __name__ == '__main__':
     # basic setting
     var_epsilon = 1  # variance for noise in linear regression
-    d = 1  # d = 5,20,100,200
-    n = int(1e5)  # sample size
-    eta = 1e-3
+    d = 11  # d = 5,20,100,200
+    n = int(1e4)  # sample size
+    eta = 1e-1
     alpha = 0.501  # step size eta_i = eta * i^{-alpha}
     x_star = np.linspace(0, 1, d)  # optimal solution
     x_0 = np.zeros(d)  # initial guess
-    R = 10  # number of bootstrap
     num_trials = 500
 
-    main_experiments(d, n, eta, alpha, x_star, x_0, R, var_epsilon, num_trials)
+    main_experiments_parallel_plug_in(d, n, eta, alpha, x_star, x_0, var_epsilon, num_trials)