fix bug. Delete wrong results.

main.py
@@ -538,7 +538,7 @@ def main_experiments_parallel_BM(d, n, eta, alpha, x_star, x_0, M_ratio, var_eps
     # SGD origial loop
     # set random seed for original samples
     M = int(np.floor(n ** (M_ratio)))-1
-    N = int(np.floor(n**(1-M_ratio)/(M+1)))
+    N = int(np.floor(n**(1-alpha)/(M+1)))
     # results = Parallel(n_jobs=32)(delayed(main_loop_BM)(seed, x_star, x_0, M, N, n, eta, var_epsilon, alpha, num_trials) for seed in range(1, 1+num_trials))
     main_loop_BM(1, x_star, x_0, M, N, n, eta, var_epsilon, alpha, num_trials)
     mean_len_history = []