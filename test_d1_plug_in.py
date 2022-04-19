7b215fab62b9e96ecbaf063e500032580059a122
wzt <wzt12345678@gmail.com>
2022-04-19 00:48:03 -0400

    experiments

--- test_d1_plug_in.py
+++ test_d1_plug_in.py
@@ -4,14 +4,14 @@ import numpy as np
 if __name__ == '__main__':
     # basic setting
     var_epsilon = 1  # variance for noise in linear regression
-    d = 11  # d = 5,20,100,200
+    # d = 11  # d = 5,20,100,200
     n = int(1e5)  # sample size
     eta = 1e-1
     alpha = 0.501  # step size eta_i = eta * i^{-alpha}
-    x_star = np.linspace(0, 1, d)  # optimal solution
-    x_0 = np.zeros(d)  # initial guess
     num_trials = 500
 
     for d in [1,5,11,200]:
         for eta in [5e-1,1e-1,5e-2,1e-2]:
+            x_star = np.linspace(0, 1, d)  # optimal solution
+            x_0 = np.zeros(d)  # initial guess
             main_experiments_parallel_plug_in(d, n, eta, alpha, x_star, x_0, var_epsilon, num_trials)
