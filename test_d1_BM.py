fix bug. Delete wrong results.

test_d1_BM.py
@@ -1,4 +1,4 @@
-from main import main_experiments, main_experiments_parallel_BM
+from main import main_experiments, main_experiments_parallel_BM,main_experiments_parallel_BM_v2
 import numpy as np
 
 if __name__ == '__main__':
@@ -14,7 +14,7 @@ if __name__ == '__main__':
         for M_ratio in [0.25]:
             for eta in [0.08,0.07,0.06,0.05,0.04,0.03]:
                 x_star = np.linspace(0, 1, d)  # optimal solution
-                x_0 = np.zeros(d)  # initial guess
-                main_experiments_parallel_BM(d, n, eta, alpha, x_star, x_0, M_ratio, var_epsilon, num_trials)
+                # x_0 = np.zeros(d)  # initial guess
+                # main_experiments_parallel_BM(d, n, eta, alpha, x_star, x_0, M_ratio, var_epsilon, num_trials)
                 x_0 = x_star
                 main_experiments_parallel_BM_v2(d, n, eta, alpha, x_star, x_0, M_ratio, var_epsilon, num_trials)