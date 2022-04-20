4a33604d3b6b77fc1d558e66323efdafc5a95edc
BeetrootWang <wzt12345678@gmail.com>
2022-04-19 23:59:14 -0400

    fix bug. Delete wrong results.

--- test_d1_BM.py
+++ test_d1_BM.py
@@ -9,11 +9,12 @@ if __name__ == '__main__':
     alpha = 0.501  # step size eta_i = eta * i^{-alpha}
     num_trials = 500
 
-    for d in [1,11,20,100,200]:
+    for d in [5]:
     # for d in [1m5]:
-        for M_ratio in [0.2, 0.25, 0.3]:
-            for eta in [1e-1,5e-2,1e-2]:
+        for M_ratio in [0.25]:
+            for eta in [0.08,0.07,0.06,0.05,0.04,0.03]:
                 x_star = np.linspace(0, 1, d)  # optimal solution
                 x_0 = np.zeros(d)  # initial guess
+                # x_0 = x_star
                 main_experiments_parallel_BM(d, n, eta, alpha, x_star, x_0, M_ratio, var_epsilon, num_trials)
                 # main_experiments_parallel_BM_v2(d, n, eta, alpha, x_star, x_0, M_ratio, var_epsilon, num_trials)
