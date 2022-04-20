5558ea253608370364f81dc7ae6fafd370233999
BeetrootWang <wzt12345678@gmail.com>
2022-04-20 01:40:22 -0400

    fix bug. Delete wrong results.

---
+++ test_wo_replacement.py
@@ -0,0 +1,19 @@
+from main import main_experiments, main_experiments_parallel_wo
+import numpy as np
+
+if __name__ == '__main__':
+    # basic setting
+    var_epsilon = 1  # variance for noise in linear regression
+    # d = 5  # d = 5,20,100,200
+    n = int(1e5)  # sample size
+    # eta = 1e-3
+    alpha = 0.501  # step size eta_i = eta * i^{-alpha}
+    # R = 2  # number of bootstrap
+    num_trials = 500
+
+    for R in [3, 5, 8]:
+        for d in [1, 5, 11, 20, 100, 200]:
+            x_star = np.linspace(0, 1, d)  # optimal solution
+            x_0 = np.zeros(d)  # initial guess
+            for eta in [1e-1,1e-2]:
+                main_experiments_parallel_wo(d, n, eta, alpha, x_star, x_0, R, var_epsilon, num_trials)
