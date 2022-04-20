97c02e00c1b4e4181abc17528611e9465ba865e2
wzt <wzt12345678@gmail.com>
2022-04-19 22:12:31 -0400

    experiments

--- test_d1.py
+++ test_d1.py
@@ -19,10 +19,10 @@ if __name__ == '__main__':
                 main_experiments_parallel(d, n, eta, alpha, x_star, x_0, R, var_epsilon, num_trials)
                 main_experiments_parallel_std(d, n, eta, alpha, x_star, x_0, R, var_epsilon, num_trials)
 
-# TODO: Go through Xi's paper; Check how they describe plug-in and batch mean estimator
+# TODO: Go through Xi's paper; Check how they describe plug-in and batch mean estimator [done]
 # TODO: R = 3,5,8 [Done]
 # TODO: without replacement experiment
-# TODO: Implement Xi's work: warm up/Sensitivity to X_0?
+# TODO: Implement Xi's work: warm up/Sensitivity to X_0? [see BM_v2, it seems that the algo is not really sensitive to initialization]
 # TODO: Proof of non-averaging case
 # TODO: without replacement proof
 # TODO: More experiments
--- test_d1_BM.py
+++ test_d1_BM.py
@@ -1,19 +1,19 @@
-from main import main_experiments, main_experiments_parallel_BM
+from main import main_experiments, main_experiments_parallel_BM, main_experiments_parallel_BM_v2
 import numpy as np
 
 if __name__ == '__main__':
     # basic setting
     var_epsilon = 1  # variance for noise in linear regression
     # d = 11  # d = 5,20,100,200
-    n = int(1e4)  # sample size
-    eta = 1e-1
+    n = int(1e5)  # sample size
     alpha = 0.501  # step size eta_i = eta * i^{-alpha}
     num_trials = 500
 
-    # for d in [1,5,11,200]:
-    for d in [2]:
-        for M_ratio in [0.25]:
-            for eta in [5e-1,1e-1,5e-2,1e-2]:
+    for d in [5]:
+    # for d in [1m5]:
+        for M_ratio in [0.2, 0.25, 0.3]:
+            for eta in [1e-1,5e-2,1e-2]:
                 x_star = np.linspace(0, 1, d)  # optimal solution
                 x_0 = np.zeros(d)  # initial guess
                 main_experiments_parallel_BM(d, n, eta, alpha, x_star, x_0, M_ratio, var_epsilon, num_trials)
+                # main_experiments_parallel_BM_v2(d, n, eta, alpha, x_star, x_0, M_ratio, var_epsilon, num_trials)
