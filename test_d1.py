fix bug. Delete wrong results.

test_d1.py
@@ -4,11 +4,12 @@ import numpy as np
 if __name__ == '__main__':
     # basic setting
     var_epsilon = 1  # variance for noise in linear regression
-    n = int(1e5)  # sample size
+    # n = int(1e5)  # sample size
     alpha = 0.501  # step size eta_i = eta * i^{-alpha}
     num_trials = 500
 
-    for R in [10]:
+    for R in [3,5,10]:
+        n = int(1e5/R)
         for d in [5,20,100,200]:
             x_star = np.linspace(0, 1, d)  # optimal solution
             x_0 = np.zeros(d)  # initial guess