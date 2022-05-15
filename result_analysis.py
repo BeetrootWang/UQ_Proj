fix bug. Delete wrong results.

result_analysis.py
@@ -1,3 +1,67 @@
 # Process the result of numerical experiments
+import numpy as np
+cnt = 0
+result_str_3 = '$R=3$  '
+result_str_5 = '$R=5$  '
+result_str_10 = '$R=10$ '
+for target_d in [5,20,200]:
+    target_n = int(1e5)
+    best_cov = [0,0,0,0] # Corresponds to R = 3,5,10,50
+    best_cov_var = [0,0,0,0]
+    best_len = [1e5, 1e5, 1e5, 1e5]
+    best_len_var = [0,0,0,0]
+    filename = f'Result_{target_d}.txt'
+    f = open(filename, 'r')
+    R_or_Ratio = np.NaN
+    for line in f:
+        if '-->' in line:
+            cnt = 1
+            if R_or_Ratio == '3':
+                ii=0
+            elif R_or_Ratio == '5':
+                ii=1
+            elif R_or_Ratio == '10':
+                ii=2
+            elif R_or_Ratio == '50':
+                ii=3
+            else:
+                continue
+            print( abs(cov-0.95) + Len )
+            print(abs(best_cov[ii] - 0.95) + best_len[ii])
+            if abs(cov-0.95) + Len < abs(best_cov[ii] - 0.95) + best_len[ii]:
+                best_cov[ii] = cov
+                best_cov_var[ii] = cov_var / np.sqrt(target_n)
+                best_len[ii] = Len
+                best_len_var[ii] = Len_var
+        elif cnt == 1:
+            cnt = 2
+            # This line contains
+            # Coverage rate and average length
+            tmp = line.split(" ")
+            cov = float(tmp[3])
+            cov_var = float(tmp[5].strip('()'))
+            Len = float(tmp[8])
+            Len_var = float(tmp[10].strip('()'))
+        elif cnt == 2:
+            cnt = 3
+            tmp = line.split()
+            d = int(tmp[1])
+            n = int(tmp[3])
+            if n!= target_n:
+                continue
+            R_or_Ratio = tmp[5]
+            eta_0 = float(tmp[7])
+            alpha = float(tmp[9])
+            num_trials = int(tmp[12])
+    result_str_3 = result_str_3 + f'& ${best_cov[0]*100:.2f}$ (${best_cov_var[0]*100:.2f}$) & ${best_len[0]*100:.2f}$ (${best_len_var[0]*100:.2f}$)'
+    result_str_5 = result_str_5 + f'& ${best_cov[1]*100:.2f}$ (${best_cov_var[1]*100:.2f}$) & ${best_len[1]*100:.2f}$ (${best_len_var[1]*100:.2f}$)'
+    result_str_10 = result_str_10 + f'& ${best_cov[2]*100:.2f}$ (${best_cov_var[2]*100:.2f}$) & ${best_len[2]*100:.2f}$ (${best_len_var[2]*100:.2f}$)'
+    # import pdb; pdb.set_trace()
+    f.close()
 
-filename = 'Result_1.txt'
+result_str_3 = result_str_3+'\\\\ \hline'
+result_str_5 = result_str_5+'\\\\ \hline'
+result_str_10 = result_str_10+'\\\\ \hline'
+print(result_str_3)
+print(result_str_5)
+print(result_str_10)

test_d1.py
@@ -9,12 +9,12 @@ if __name__ == '__main__':
     num_trials = 500
 
     for R in [3,5,10]:
-        n = int(1e5/R)
+        # n = int(1e5/R)
         for d in [5,20,100,200]:
             x_star = np.linspace(0, 1, d)  # optimal solution
             x_0 = np.zeros(d)  # initial guess
             for eta in [5e-1,1e-1,5e-2,1e-2]:
-                for cov_a_str in ['toeplitz','equi']:
+                for cov_a_str in ['identity']:
                     main_experiments_parallel(d, n, eta, alpha, x_star, x_0, R, var_epsilon, cov_a_str, num_trials)
                     main_experiments_parallel_std(d, n, eta, alpha, x_star, x_0, R, var_epsilon, cov_a_str, num_trials)
 

test_d1_plug_in.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
 
     for d in [5, 20, 100, 200]:
         for eta in [5e-1,1e-1,5e-2,1e-2]:
-                for cov_a_str in ['toeplitz','equi']:
+                for cov_a_str in ['identity']:
                     x_star = np.linspace(0, 1, d)  # optimal solution
                     x_0 = np.zeros(d)  # initial guess
                     main_experiments_parallel_plug_in(d, n, eta, alpha, x_star, x_0, var_epsilon, cov_a_str, num_trials)