fix bug. Delete wrong results.

result_analysis_Xi.py
@@ -1,20 +1,21 @@
 # Process the result of numerical experiments
 import numpy as np
 cnt = 0
-result_str = 'Plug-in '
+result_str = 'Batch Mean '
 for target_d in [5,20,200]:
     target_n = int(1e5)
     best_cov = 0 # Corresponds to R = 3,5,10,50
     best_cov_var = 0
     best_len = 1e5
     best_len_var = 0
-    filename = f'Result_PI_{target_d}.txt'
+    filename = f'Result_BM_{target_d}_identity.txt'
+    print(filename)
     f = open(filename, 'r')
     R_or_Ratio = 'something wrong'
     for line in f:
-        if '-->' in line and R_or_Ratio!='something wrong':
+        if '-->' in line:
             cnt = 1
-            if abs(cov-0.95) + Len < abs(best_cov - 0.95) + best_len:
+            if R_or_Ratio!='something wrong' and abs(cov-0.95) + Len < abs(best_cov - 0.95) + best_len:
                 best_cov = cov
                 best_cov_var = cov_var / np.sqrt(target_n)
                 best_len = Len
@@ -33,12 +34,22 @@ for target_d in [5,20,200]:
             tmp = line.split()
             d = int(tmp[1])
             n = int(tmp[3])
-            if n!= target_n:
-                continue
-            R_or_Ratio = tmp[5]
-            eta_0 = float(tmp[7])
-            alpha = float(tmp[9])
-            num_trials = int(tmp[12])
+            if 'BM' in filename:
+                if n!= target_n:
+                    continue
+                R_or_Ratio = tmp[5+1]
+                eta_0 = float(tmp[7+1])
+                alpha = float(tmp[9+1])
+                num_trials = int(tmp[12+1])
+            else:
+                if n!= target_n:
+                    cov=0
+                    Len=1e5
+                    continue
+                R_or_Ratio = tmp[5]
+                eta_0 = float(tmp[7])
+                alpha = float(tmp[9])
+                num_trials = int(tmp[12])
     result_str = result_str + f'& ${best_cov*100:.2f}$ (${best_cov_var*100:.2f}$) & ${best_len*100:.2f}$ (${best_len_var*100:.2f}$)'
     # import pdb; pdb.set_trace()
     f.close()

result_analysis_fair.py
@@ -5,7 +5,7 @@ result_str_3 = '$R=3$  '
 result_str_5 = '$R=5$  '
 result_str_10 = '$R=10$ '
 for target_d in [5,20,200]:
-    target_n = int(1e5)
+    target_Rn = int(1e5)
     best_cov = [0,0,0,0] # Corresponds to R = 3,5,10,50
     best_cov_var = [0,0,0,0]
     best_len = [1e5, 1e5, 1e5, 1e5]
@@ -13,7 +13,6 @@ for target_d in [5,20,200]:
     filename = f'Result_{target_d}_identity.txt'
     f = open(filename, 'r')
     R_or_Ratio = np.NaN
-    ii=1
     for line in f:
         if '-->' in line:
             cnt = 1
@@ -22,12 +21,17 @@ for target_d in [5,20,200]:
             elif R_or_Ratio == '5':
                 ii=1
             elif R_or_Ratio == '10':
-                # import pdb; pdb.set_trace()
                 ii=2
             elif R_or_Ratio == '50':
                 ii=3
             else:
                 continue
+            if abs(cov-0.95) + Len < abs(best_cov[ii] - 0.95) + best_len[ii]:
+                print(n, R_or_Ratio, target_Rn)
+                best_cov[ii] = cov
+                best_cov_var[ii] = cov_var / np.sqrt(n)
+                best_len[ii] = Len
+                best_len_var[ii] = Len_var
         elif cnt == 1:
             cnt = 2
             # This line contains
@@ -42,17 +46,14 @@ for target_d in [5,20,200]:
             tmp = line.split()
             d = int(tmp[1])
             n = int(tmp[3])
-            if n!= target_n:
-                continue
             R_or_Ratio = tmp[5]
+            if n!= int(target_Rn/int(R_or_Ratio)):
+                cov=0
+                Len=1e5
+                continue
             eta_0 = float(tmp[7])
             alpha = float(tmp[9])
             num_trials = int(tmp[12])
-        if abs(cov-0.95) + Len < abs(best_cov[ii] - 0.95) + best_len[ii]:
-            best_cov[ii] = cov
-            best_cov_var[ii] = cov_var / np.sqrt(target_n)
-            best_len[ii] = Len
-            best_len_var[ii] = Len_var
     result_str_3 = result_str_3 + f'& ${best_cov[0]*100:.2f}$ (${best_cov_var[0]*100:.2f}$) & ${best_len[0]*100:.2f}$ (${best_len_var[0]*100:.2f}$)'
     result_str_5 = result_str_5 + f'& ${best_cov[1]*100:.2f}$ (${best_cov_var[1]*100:.2f}$) & ${best_len[1]*100:.2f}$ (${best_len_var[1]*100:.2f}$)'
     result_str_10 = result_str_10 + f'& ${best_cov[2]*100:.2f}$ (${best_cov_var[2]*100:.2f}$) & ${best_len[2]*100:.2f}$ (${best_len_var[2]*100:.2f}$)'