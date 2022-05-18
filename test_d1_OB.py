f0aa2e06cda39c09e9e12f8f29bb7764a37ee7a8
BeetrootWang <wzt12345678@gmail.com>
2022-05-18 00:38:10 -0400

    fix bug. Delete wrong results.

---
+++ test_d1_OB.py
@@ -0,0 +1,20 @@
+from main import main_experiments_parallel_OB
+import numpy as np
+# Experiment for Online Bootstrap
+if __name__ == '__main__':
+    # basic setting
+    var_epsilon = 1  # variance for noise in linear regression
+    # d = 11  # d = 5,20,100,200
+    n = int(1e5)  # sample size
+    alpha = 0.501  # step size eta_i = eta * i^{-alpha}
+    num_trials = 12
+    B = 100
+
+    for d in [5]:
+        # for eta in np.linspace(0.02,0.1,8):
+        for eta in [0.05]:
+            # for cov_a_str in ['identity','toeplitz','equi']:
+            for cov_a_str in ['identity']:
+                x_star = np.linspace(0, 1, d)  # optimal solution
+                x_0 = np.zeros(d)  # initial guess
+                main_experiments_parallel_OB(d, n, eta, alpha, x_star, x_0, B, var_epsilon, cov_a_str, num_trials)
